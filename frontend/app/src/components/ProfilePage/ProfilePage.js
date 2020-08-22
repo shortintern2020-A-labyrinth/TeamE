2e61f30015db4e81f7e04233faf0e2914c60d7f4
yujialves <yujialves@i.softbank.jp>
2020-08-23 01:00:07 +0900

    ProfilePageコンポーネントの作成

--- frontend/app/src/components/ProfilePage/ProfilePage.js
+++ frontend/app/src/components/ProfilePage/ProfilePage.js
@@ -1,7 +1,32 @@
 import React from "react";
+import styles from "./ProfilePage.module.css";
+import ArtistsView from "./ArtistsView/ArtistsView";
+import ProfileView from "./ProfileView/ProfileView";
 
 const ProfilePage = () => {
-  return <div>ProfilePage</div>;
+  const favorites = [
+    { name: "嵐" },
+    { name: "Twice" },
+    { name: "Ed Sheeran" },
+    { name: "乃木坂46" },
+    { name: "BTS" },
+    { name: "King gnu" },
+    { name: "Taylor Swift" },
+  ];
+
+  return (
+    <div className={styles.container}>
+      <ProfileView
+        name="楽天パンダ"
+        description="都内の大学生です！幅広く聞きます！よろしくお願いします！"
+      />
+      <ArtistsView
+        favorites={favorites}
+        onEdit={() => {}}
+        onDelete={() => {}}
+      />
+    </div>
+  );
 };
 
 export default ProfilePage;
