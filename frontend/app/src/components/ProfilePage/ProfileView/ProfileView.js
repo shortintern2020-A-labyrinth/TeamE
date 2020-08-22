8db95949a2117eff8ca2304fab8239a6d066c04a
yujialves <yujialves@i.softbank.jp>
2020-08-23 00:53:52 +0900

    ProfileViewコンポーネントの作成

---
+++ frontend/app/src/components/ProfilePage/ProfileView/ProfileView.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from './ProfileView.module.css'
+
+const ProfileView = (props) => {
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1 className={styles.title}>Profile</h1>
+        <div className={styles.profile}>
+          <div className={styles.header}>
+            <img
+              src=""
+              className={styles.img}
+            />
+            <h2 className={styles.name}>{props.name}</h2>
+          </div>
+          <p className={styles.description}>
+            {props.description}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileView;
