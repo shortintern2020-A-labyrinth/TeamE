890c687cbcdfc56b6cc1c7d00a264a5c8418cba0
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-08-22 15:05:08 +0900

    try to use hooks

--- frontend/app/src/components/ArtistListPage/ArtistListPage.js
+++ frontend/app/src/components/ArtistListPage/ArtistListPage.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ArtistListPage.css';
 
 const ArtistListPage = () => {
+  const [value, setValue] = useState("aaa");
+
   return (
     <div>
       <h1>ArtistListPage</h1>
+      <p>{value}</p>
+      <input type="button" onClick={() => setValue("bbb")} />
     </div>
   );
 };
