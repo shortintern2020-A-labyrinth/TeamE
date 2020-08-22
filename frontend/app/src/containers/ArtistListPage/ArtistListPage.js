7d2e6c77887283f9378c618402a20b16cc8d3fad
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-08-22 09:42:20 +0900

    Add SemanticUI, new ICON and css files in containers

---
+++ frontend/app/src/containers/ArtistListPage/ArtistListPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './ArtistListPage.css';
+
+const ArtistListPage = () => {
+  return (
+    <div>
+      <h1>ArtistListPage</h1>
+    </div>
+  );
+};
+
+export default ArtistListPage;
