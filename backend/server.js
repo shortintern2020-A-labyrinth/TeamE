1c14112c5241cb53017266c105e4f47041c6ea42
magnusbarata <barata.magnuz@gmail.com>
2020-08-25 14:29:52 +0900

    add cors to server.js

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,12 @@
 
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 const app = express();
+app.use(cors());
+
 app.get('/', (req, res) => {
   res.send('This is Amato Music Server');
 });
