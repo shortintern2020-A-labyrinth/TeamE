39045ee2a2f017901f4d3eda28b37839e5fa9e2e
yujialves <yujialves@i.softbank.jp>
2020-08-25 19:06:26 +0900

    Implpement redux-thunk

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -4,15 +4,16 @@ import LandingPage from "./components/LandingPage/LandingPage";
 import ProfilePage from "./components/ProfilePage/ProfilePage";
 import ArtistListPage from "./components/ArtistListPage/ArtistListPage";
 import ChatPage from "./components/ChatPage/ChatPage";
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
+import ReduxThunk from "redux-thunk";
 import authReducer from "./store/reducers/authReducer";
 import "./App.css";
 
 const rootReducer = combineReducers({
   auth: authReducer,
 });
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(ReduxThunk));
 
 function App() {
   return (
