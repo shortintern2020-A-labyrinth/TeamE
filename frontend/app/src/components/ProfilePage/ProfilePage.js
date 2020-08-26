ba7af7ae93934da74f56e366d1b1cd1fcc680eca
yujialves <yujialves@i.softbank.jp>
2020-08-26 11:01:12 +0900

    Fix the profile picture bug

--- frontend/app/src/components/ProfilePage/ProfilePage.js
+++ frontend/app/src/components/ProfilePage/ProfilePage.js
@@ -59,7 +59,7 @@ const ProfilePage = () => {
         <ProfileView
           name={userData.user}
           description={userData.selfIntro}
-          imgSrc={userData.images}
+          imgSrc={userData.images.url}
         />
         <ArtistsView
           favorites={userData.likedArtists}
