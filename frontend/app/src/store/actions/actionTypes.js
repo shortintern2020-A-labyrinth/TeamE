653efb0cfab05490a18040a459c19d93279db0b8
yujialves <yujialves@i.softbank.jp>
2020-08-26 14:08:52 +0900

    Implement artist reducer

--- frontend/app/src/store/actions/actionTypes.js
+++ frontend/app/src/store/actions/actionTypes.js
@@ -3,3 +3,4 @@ export const SET_LOADING = "SET_LOADING";
 export const SET_USER = "SET_USER";
 export const SET_NEXT_PAGE = "SET_NEXT_PAGE";
 export const SET_LIKED_ARTISTS = "SET_LIKED_ARTISTS";
+export const SET_ARTIST = "SET_ARTIST";
